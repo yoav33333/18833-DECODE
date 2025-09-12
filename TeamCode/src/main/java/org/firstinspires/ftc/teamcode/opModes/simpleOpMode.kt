d17386b93ac66db6ab492c77fd57105fefc780a9
yoav33333 <yoav.2009.goren@gmail.com>
2025-09-12 12:58:25 +0300

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/simpleOpMode.kt
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opModes
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+
+class simpleOpMode: OpMode() {
+
+    override fun init() {
+
+    }
+
+    override fun loop() {
+
+    }
+}
