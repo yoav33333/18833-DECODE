413b08ad3d2af66c2729576614af496856694cc0
yoav33333 <yoav.2009.goren@gmail.com>
2025-09-12 13:26:33 +0300

    added dairy and pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/simpleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/simpleOpMode.kt
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.opModes
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 
+@TeleOp
 class simpleOpMode: OpMode() {
 
     override fun init() {
