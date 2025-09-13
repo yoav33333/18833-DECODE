22991fd963391994ac7dc36b32d6545fc31fe016
guyka2212 <2212kadosh@gmail.com>
2025-09-13 14:40:37 +0300

    added MecanumDrive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/MecanumDrive.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+
+public class MecanumDrive extends OpMode {
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+    private IMU imu;
+    @Override
+    public void init() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "flm");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "blm");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frm");
+        backRightMotor = hardwareMap.get(DcMotor.class, "brm");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        double frontLeftPower = forward + strafe + rotate;
+        double backLeftPower = forward - strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double backRightPower = forward + strafe - rotate;
+
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        frontLeftMotor.setPower(maxSpeed * (frontLeftPower / maxPower));
+        backLeftMotor.setPower(maxSpeed * (backLeftPower / maxPower));
+        frontRightMotor.setPower(maxSpeed * (frontRightPower / maxPower));
+        backRightMotor.setPower(maxSpeed * (backRightPower / maxPower));
+    }
+
+    @Override
+    public void loop() {
+        drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/ReadRobotTeleopDrive.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ReadRobotTeleopDrive extends OpMode {
+
+
+    @Override
+    public void init() {
+
+    }
+//   to now every button/joy stick mode
+    @Override
+    public void loop() {
+        telemetry.addData("lx", gamepad1.left_stick_x);
+        telemetry.addData("ly", gamepad1.left_stick_y);
+        telemetry.addData("rx", gamepad1.right_stick_x);
+        telemetry.addData("ry", gamepad1.right_stick_y);
+        telemetry.addData("a", gamepad1.a);
+        telemetry.addData("x", gamepad1.x);
+        telemetry.addData("y", gamepad1.y);
+        telemetry.addData("b", gamepad1.b);
+    }
+}
